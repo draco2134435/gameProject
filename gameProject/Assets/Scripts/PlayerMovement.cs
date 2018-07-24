41abdd2186e5e16abd08f0d291de147455f8e41c
draco2134435 <ms3990@g.rit.edu>
2018-07-24 11:55:35 -0400

    Added shootig, movement, and jump with groundcheck in progress

---
+++ gameProject/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerMovement: MonoBehaviour {
+
+	[SerializeField] private Transform groundCheck;
+	[SerializeField] private Transform ceilingCheck;
+	public float jumpForce = 5000f;
+	public float maxSpeed = 10f;
+	public bool facingRight = true;
+	private Rigidbody2D rigBody;
+	private float lastDistance;
+	private LayerMask environmentLayerMask;
+
+	void awake(){
+		
+	}
+	void start(){
+		
+	}
+
+	void FixedUpdate(){
+		environmentLayerMask = LayerMask.GetMask ("Environment");
+		rigBody = GetComponent<Rigidbody2D>();
+		float move = Input.GetAxis("Horizontal");
+		rigBody.velocity = new Vector2 (move * maxSpeed, rigBody.velocity.y);
+		if (move > 0 && !facingRight) {
+			flip ();
+		} else if (move < 0 && facingRight) {
+			flip ();
+		}
+		float jumpAxis = Input.GetAxisRaw ("Jump");
+		if (jumpAxis != 0 && rigBody.velocity.y <= 0) {
+			RaycastHit2D hit2D = Physics2D.Raycast (rigBody.position - new Vector2 (0f, 0.5f), Vector2.down, 0.2F, environmentLayerMask);
+
+			if (hit2D) {
+				Debug.Log ("Hello1");
+				if (hit2D.distance < lastDistance) {
+					lastDistance = hit2D.distance;
+				} else {
+					Debug.Log ("Hello");
+					lastDistance = 100f;
+					jump (jumpAxis * jumpForce * Time.deltaTime);
+				}
+			}
+		}
+	}
+	void flip(){
+		facingRight = !facingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+
+	}
+	void jump(float force){
+		if (jumpForce < 0) {
+			return;
+		}
+		rigBody.AddForce (new Vector2 (0, force));
+	}
+}
---
+++ gameProject/Assets/Scripts/weapon.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class weapon : MonoBehaviour {
+	public float fireRate = 0f;
+	public float dmg = 10f;
+	public LayerMask notToHit;
+	public bool facingRight = true;
+
+	float timeToFire = 0f;
+	Transform firePoint;
+	// Use this for initialization
+	void awake(){
+		firePoint = transform.Find ("firePoint");
+		if (firePoint == null) {
+			Debug.Log ("There is no firepoint");
+		}
+	}
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update(){
+	}
+	void FixedUpdate () {
+		
+		if (Input.GetButton ("Vertical")) {	
+			
+			transform.eulerAngles = new Vector3 (0f, 0f, 90f * Input.GetAxisRaw ("Vertical"));
+		} else {
+			transform.eulerAngles = new Vector3 (0f, 0f, 0f);
+		}
+
+		if (fireRate == 0) {
+			if (Input.GetButtonDown ("Fire3")) {
+				shoot ();
+			}
+		} else {
+			//add for cooldown.
+		}
+
+	}
+
+	void shoot(){
+		Debug.Log ("Shooting is working");
+	}
+}

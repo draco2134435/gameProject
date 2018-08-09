fd7ee2fd4d22a4198bc04c1d7276a6e13a4b5d09
draco2134435 <ms3990@g.rit.edu>
2018-08-09 09:19:23 -0400

    The actual commit, added all week one tasks and they are fully completed.

---
+++ gameProject/Assets/Scripts/moveBullet.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveBullet : MonoBehaviour {
+    private GameObject myObject;
+    public int moveSpeed = 230;
+    private bool facingRight;
+	// Use this for initialization
+	void Start () {
+        //get the direction
+        myObject = GameObject.Find("mainCharacter");
+        facingRight = myObject.GetComponent<PlayerMovement>().facingRight;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (facingRight)
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * moveSpeed);
+            Destroy(gameObject, 1);
+        }
+        else
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * moveSpeed);
+            Destroy(gameObject, 1);
+        }
+        
+
+     }
+}
